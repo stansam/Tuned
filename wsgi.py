c898de5353b51e6317f263a3c3f8cf6671c046f6
stansam <stanleycmam122@gmail.com>
2025-09-18 23:25:13 +0300

    Edited wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,5 +6,5 @@ app = create_app()
 application = app
 
 if __name__ == "__main__":
-    # socketio.run(app, debug=False, host="0.0.0.0", port=5000)
-    socketio.run(app, debug=False, host="127.0.0.1", port=5000)
+    socketio.run(app, debug=False, host="0.0.0.0", port=5000)
+    # socketio.run(app, debug=False, host="127.0.0.1", port=5000)
