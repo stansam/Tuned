5f450363e3781d571dc205196b87c6c5ec63a9c7
stansam <stanleycmam122@gmail.com>
2025-09-18 22:15:02 +0300

    Added wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,8 @@ from app.extensions import socketio
 
 app = create_app()
 
-if __name__ == '__main__':
-    # app.run(debug=True)
+application = app
+
+if __name__ == "__main__":
+    # socketio.run(app, debug=False, host="0.0.0.0", port=5000)
     socketio.run(app, debug=False, host="127.0.0.1", port=5000)
